be3bc5241898c6dbfa1d1b8802c96d494e776313
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-05-06 23:00:02 -0300

    Updates .gitignore

--- promoproducts/models.py
+++ promoproducts/models.py
@@ -2,20 +2,24 @@ from peewee import *
 
 db = SqliteDatabase('promo.db')
 
+
 class Store(Model):
     id = PrimaryKeyField()
     name = CharField()
     url = CharField()
+
 
 class Department(Model):
     name = CharField()
     url = CharField()
     stores = ForeignKeyField(Store)
 
+
 class Category(Model):
     name = CharField()
     url = CharField()
     departments = ForeignKeyField(Department)
+
 
 class Product(Model):
     name = CharField()
@@ -26,13 +30,13 @@ class Product(Model):
     is_available = IntegerField()
     categories = ForeignKeyField(Category)
 
+
 class Coupon(Model):
     id = PrimaryKeyField()
     codigo = CharField()
     stores = ForeignKeyField(Store)
 
+
 def create_tables():
     db.connect()
     db.create_tables([Store, Department, Category, Product, Coupon])
-
-import pdb; pdb.set_trace()
